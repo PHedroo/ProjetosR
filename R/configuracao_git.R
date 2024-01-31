4c39a8ed99daf5bf44bbefb9101dcc000f5392dd
phedroo <pedro.marucio@gmail.com>
2024-01-31 08:39:10 -0300

    primeiro passo para configurar o git

--- R/configuracao_git.R
+++ R/configuracao_git.R
@@ -1,9 +1,14 @@
-install.packages("tidyverse")
-install.packages("usethis")
-install.packages("geobr")
-install.packages("trend")
+# REFERENCIA: https://beatrizmilz.github.io/RLadies-Git-RStudio-2019/#1
 
-usethis::use_git_config(user.name = "PHedroo", # Seu nome
+# install.packages("tidyverse")
+install.packages("usethis")
+# install.packages("geobr")
+# install.packages("trend")
+
+usethis::use_git_config(user.name = "phedroo", # Seu nome
                         user.email = "pedro.marucio@gmail.com") # Seu email
-usethis::create_github_token()
+
+usethis::create_github_token() #funcao nova (referindo-se ao artigo de 2019)
 usethis::edit_r_environ()
+
+
