5a7d921c37164c5c0652180d3afccd31e5ffa5dd
phedroo <pedro.marucio@gmail.com>
2024-01-31 10:13:09 -0300

    aplicando_funcoes_mutate_filter_groupby_summarise

--- R/aplicando_funcoes_mutate_filter_groupby_summarise.R
+++ R/aplicando_funcoes_mutate_filter_groupby_summarise.R
@@ -1,12 +1,20 @@
 install.packages("dplyr")
+install.packages("geobr")
+install.packages("trend")
 install.packages("tidyverse") #engloba o pacote lubridate (maniulação de datas)
 # install.packages()
 
 library(dplyr)
 library(tidyverse)
+library(geobr)
+library(trend)
 
+#LENDO O BANCO DE DADOS IMPORTADO PARA A PASTA data-raw
+  #BANCO DE DADOS ESTE EM FORMATO .csv, nomeado "xco2"
 df <- read.csv('data-raw/xco2.csv')
 df
+
+# View(df) or click in df on environment
 
 # USANDO PIPE
 df %>%
