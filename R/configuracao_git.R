6e3056124f2ef71eb078182a867aa29045a71b0b
Pedro Oliveira <158082412+phedroo@users.noreply.github.com>
2025-01-21 23:03:24 -0300

    configuracao_git.R

--- R/configuracao_git.R
+++ R/configuracao_git.R
@@ -3,7 +3,7 @@ install.packages("usethis")
 install.packages("geobr")
 install.packages("trend")
 
-usethis::use_git_config(user.name = "PHedroo", # Seu nome
-                        user.email = "pedro.marucio@gmail.com") # Seu email
+usethis::use_git_config(user.name = "seu_username_github", # Seu nome
+                        user.email = "seu_email_github") # Seu email
 usethis::create_github_token()
 usethis::edit_r_environ()
